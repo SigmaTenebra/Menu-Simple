c3d0826bf1b6fa6c24edc8293d0f8a46af29679a
Bruno Perg <perg.bruno.mereles@gmail.com>
2022-05-30 23:39:49 -0300

    segundo desafio

--- js/main.js
+++ js/main.js
@@ -1,49 +1,63 @@
-let opcion=99;
-let option=99;
+let opcionA;
+let opcionB;
 let total=0;
+
+function menuPrincipal(){
+    let option= parseInt(prompt("Lorem \n\nMenu: \n1:Comprar \n2:Carrito \n0:Finalizar Compra \n\nIngresar el numero de la opcion deseada:"));
+    return option;
+}
+function menuCompra(){
+    let option = parseInt(prompt("Lorem \n\nProductos: \n1:God of War 2 - Valor $1000 \n2:Resident Evil 4 - Valor $800\n3:Gran Turismo 4 - Valor $600 \n4:Mortal Kombat: Shaolin Monks - Valor $700\n5:UFC3 - Valor $900 \n6:Tekken 5 - Valor $500 \n7:Black - Valor $700 \n0:Menu Anterior \n\nIngresar el numero del juego deseado:"));
+    switch (option){
+        case 1:
+            pushCarrito(1000,"God of War 2");
+            break;
+        case 2:
+            pushCarrito(800,"Resident Evil 4");
+            break;
+        case 3:
+            pushCarrito(600,"Gran Turismo 4");
+            break;
+        case 4:
+            pushCarrito(700,"Mortal Kombat: SM");
+            break;
+        case 5:
+            pushCarrito(900,"UFC 3");
+            break;
+        case 6:
+            pushCarrito(500,"Tekken 5");
+            break;
+        case 7:
+            pushCarrito(700,"Black");
+            break;
+        case 0:
+            break;                    
+        default:
+            alert("Opcion ingresada no valida!!");
+            break;
+    }
+    return option;
+}
+function pushCarrito(num,name){
+    total+=num;
+    alert("Lorem\n\nSe agrego "+ name +" a su carrito, por un valor de "+num+"\nEl coste total de su compra es de: "+total);
+}
+function carrito(){
+    alert("Lorem \n\nEl coste total de su compra es de: "+total);
+}
 
 alert("Bienvenido a Lorem");
 do{
-    opcion= parseInt(prompt("Lorem \n\nMenu: \n1:Comprar \n2:Lista de Productos \n3:Carrito \n0:Finalizar Compra \n\nIngresar el numero de la opcion deseada:"));
-    switch(opcion){
+    opcionA=menuPrincipal();
+    switch(opcionA){
         case 1:
             do{
-                option= parseInt(prompt("Lorem \n\nProductos: \n1:God of War 2 \n2:Resident Evil 4 \n3:Gran Turismo 4 \n4:Mortal Kombat: Shaolin Monks \n5:UFC3 \n6:Tekken 5 \n7:Black \n0:Menu Anterior \n\nIngresar el numero del juego deseado:"));
-                switch (option){
-                    case 1:
-                        total+=1000;
-                        break;
-                    case 2:
-                        total+=800;
-                        break;
-                    case 3:
-                        total+=600;
-                        break;
-                    case 4:
-                        total+=700;
-                        break;
-                    case 5:
-                        total+=900;
-                        break;
-                    case 6:
-                        total+=500;
-                        break;
-                    case 7:
-                        total+=700;
-                        break;
-                    case 0:
-                        break;                    
-                    default:
-                        alert("Opcion ingresada no valida!!");
-                        break;
-                }
-            }while(option!=0);
+                opcionB= menuCompra();
+            }while(opcionB!=0);
+            carrito();
             break;
         case 2:
-            alert("Lorem \n\nGod of War 2 - Valor $1000\nResident Evil 4 - Valor $800 \nGran Turismo 4 - Valor $600 \nMortal Kombat: SM - Valor $700 \nSmack vs Raw - Valor $900 \nTekken 5 - Valor $500 \nBlack - Valor $700");
-            break;
-        case 3:
-            alert("Lorem \n\nEl coste total de su compra es de: "+total);
+            carrito();
             break;
         case 0:
             break;
@@ -51,5 +65,6 @@ do{
             alert("Opcion ingresada no valida!!");
             break;
     }
-}while(opcion!=0);
+}while(opcionA!=0);
 alert("Lorem \n\nEl coste total de su compra es de: "+total+"\n\nGracias por utilizar nuestro sistema de compra!!\nVuelva pronto!!");
+

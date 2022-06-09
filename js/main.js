182ce39ffefa87cb7215cca20fe8d19607ce5101
Bruno Perg <perg.bruno.mereles@gmail.com>
2022-06-08 23:36:43 -0300

    actualizacion de java

--- js/main.js
+++ js/main.js
@@ -1,18 +1,40 @@
+class Juego {
+    constructor(nombre, precio){
+        this.nombre = nombre;
+        this.precio = "$"+ precio;
+        this.valor = Number(precio);
+    }
+    agregandoIVA(){
+        this.valor=this.valor*1.21;
+    }
+}
+const listaJuegos = [];
+listaJuegos.push(new Juego("God of War 2",1000));
+listaJuegos.push(new Juego("Resident Evil 4",800));
+listaJuegos.push(new Juego("Gran Turismo 4",600));
+listaJuegos.push(new Juego("Mortal Kombat: SM",700));
+listaJuegos.push(new Juego("UFC3",900));
+listaJuegos.push(new Juego("Tekken 5",500));
+listaJuegos.push(new Juego("Black",700));
+for(const juego of listaJuegos){
+    juego.agregandoIVA();
+}
+
+
+const carritoLi = [];
+const carritoValor = [];
+const carritoIndex = [];
+
 let opcionA;
 let opcionB;
 let total=0;
-const juegos = ["God of War 2", "Resident Evil 4", "Gran Turismo 4", "Mortal Kombat: SM", "UFC3", "Tekken 5", "Black"];
-const precios = ["$1000", "$800", "$600", "$700", "$900", "$500", "$700"];
-const valor = [1000,800,600,700,900,500,700];
-const carritoLi = [];
-const carritoValor = [];
 
 function menuPrincipal(){
     let option= parseInt(prompt("Lorem \n\nMenu: \n1:Comprar \n2:Carrito \n0:Finalizar Compra \n\nIngresar el numero de la opcion deseada:"));
     return option;
 }
 function menuCompra(){
-    let option = parseInt(prompt("Lorem \n\nProductos: \n1:" + juegos[0] + " - Valor "+ precios[0] +" \n2:" + juegos[1] + " - Valor "+ precios[1] +"\n3:" + juegos[2] + " - Valor "+ precios[2] +" \n4:" + juegos[3] + " - Valor "+ precios[3] +"\n5:" + juegos[4] + " - Valor "+ precios[4] +" \n6:" + juegos[5] + " - Valor "+ precios[5] +" \n7:" + juegos[6] + " - Valor "+ precios[6] +" \n0:Menu Anterior \n\nIngresar el numero del juego deseado:"));
+    let option = parseInt(prompt("Lorem \n\nProductos: \n1:" + listaJuegos[0].nombre + " - Valor "+ listaJuegos[0].precio +"+IVA \n2:" + listaJuegos[1].nombre + " - Valor "+ listaJuegos[1].precio +"+IVA\n3:" + listaJuegos[2].nombre + " - Valor "+ listaJuegos[2].precio +"+IVA \n4:" + listaJuegos[3].nombre + " - Valor "+ listaJuegos[3].precio +"+IVA\n5:" + listaJuegos[4].nombre + " - Valor "+ listaJuegos[4].precio +"+IVA \n6:" + listaJuegos[5].nombre + " - Valor "+ listaJuegos[5].precio +"+IVA \n7:" + listaJuegos[6].nombre + " - Valor "+ listaJuegos[6].precio +"+IVA \n0:Menu Anterior \n\nIngresar el numero del juego deseado:"));
     switch (option){
         case 1:
             pushCarrito(0);
@@ -44,26 +66,37 @@ function menuCompra(){
     return option;
 }
 function pushCarrito(indice){
-    total+=valor[indice];
-    alert("Lorem\n\nSe agrego "+ juegos[indice] +" a su carrito, por un valor de "+precios[indice]+"\nEl coste total de su compra es de: "+total);
-    if(carritoLi.includes(juegos[indice])){
-        carritoValor[carritoLi.indexOf(juegos[indice])]+=1;
+    total+=listaJuegos[indice].valor;
+    alert("Lorem\n\nSe agrego "+ listaJuegos[indice].nombre +" a su carrito, por un valor de "+listaJuegos[indice].precio+"+IVA\nEl coste total de su compra es de: "+total);
+    if(carritoLi.includes(listaJuegos[indice].nombre)){
+        carritoValor[carritoLi.indexOf(listaJuegos[indice].nombre)]+=1;
     }else{
-        carritoLi.push(juegos[indice]);
+        carritoLi.push(listaJuegos[indice].nombre);
         carritoValor.push(1);
+        carritoIndex.push(indice);
     }
 }
 function carrito(){
     if(carritoLi.length !=0){
-        for(let i=0; i<carritoLi.length ;i++){
-            let name = carritoLi[i];
-            let indice = juegos.indexOf(name);
-            alert("Lorem \n\n     "+carritoLi[i]+"   *"+ carritoValor[i] +"   Valor:"+ carritoValor[i]*valor[indice]);
-        }
-        alert("Lorem \n\nEl coste total de su compra es de: "+total);
+        let mensajeCarrito=armadoMensaje();
+        
+        mensajeCarrito+="\nEl coste total de su compra es de: "+total;
+        alert(mensajeCarrito);
     }else{
         alert("Lorem \n\nSu carrito de compra esta vacia");
     }
+}
+function armadoMensaje(){
+    let mensaje = "Lorem\n\n";
+    for(let i=0; i<carritoLi.length ;i++){
+        mensaje+="     "+carritoLi[i]+"   *"+ carritoValor[i] +"   Valor:"+ carritoValor[i]*listaJuegos[carritoIndex[i]].valor+"\n";
+    }
+    return mensaje;
+}
+function mensajeSalida(){
+    let mensajeFinal = armadoMensaje();
+    mensajeFinal+="\nEl coste total de su compra es de: "+total+"\n\nGracias por utilizar nuestro sistema de compra!!\nVuelva pronto!!";
+    alert(mensajeFinal);
 }
 
 alert("Bienvenido a Lorem");
@@ -86,5 +119,4 @@ do{
             break;
     }
 }while(opcionA!=0);
-alert("Lorem \n\nEl coste total de su compra es de: "+total+"\n\nGracias por utilizar nuestro sistema de compra!!\nVuelva pronto!!");
-
+mensajeSalida();
